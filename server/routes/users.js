c8fa2519a89030f358df0313bdb488f7da49209c
YudhistiraM <ma.yudhistira@gmail.com>
2018-11-19 06:32:06 +0700

    updated-server Api

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,6 +9,8 @@ router.get('/register', function(req, res, next) {
 });
 
 router.post('/register', function(req, res, next){
+  // let countMail = User.find({email: req.body.email}).count();
+  // console.log(countMail);
   if (req.body.password == req.body.retypepassword) {
     const user = new User({
       email: req.body.email,
@@ -34,6 +36,47 @@ router.post('/register', function(req, res, next){
         message: 'password and retypepassword are not match'
       })
     }
+  })
+
+  router.post('/login', (req, res) =>{
+    User.findOne({
+      email: req.body.email,
+    }).then(user =>{
+      if (!user){
+        res.json({error:true, message : "Email is invalid"})
+      } else {
+        if (req.body.password != user.password) {
+          res.json({error: true, message:"Password is invalid"})
+        }else {
+          let token = jwt.sign({ user:user}, 'secretkey', {expiresIn: 86400});
+          res.json({
+            data:{
+              email: user.email
+            },
+            token: token
+          })
+        }
+      }
+    })
+  })
+
+  // check
+  router.post('/check', (req, res) => {
+    var token = req.body.token || req.query.token || req.header['x-access-token'];
+    jwt.verify(token, 'secretkey', (err, decoded) =>{
+      if (decoded){
+        res.json({valid: true})
+      }else {
+        res.json({valid: false, message: "Token is invalid"})
+      }
+    })
+  })
+
+  // destroy
+  router.get('/destroy', (req, res) =>{
+    res.json({
+      logout: true
+    })
   })
 
 
